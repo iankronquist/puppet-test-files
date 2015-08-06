503ba473995dd167405183cbe15a0500c32025d4
Ian Kronquist <iankronquist@teleport.com>
2015-08-06 13:27:07 -0700

    Add more files

---
+++ custom_type.rb
@@ -0,0 +1,13 @@
+Puppet::Type.newtype(:custom_package) do
+  def exists?
+  
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+end
---
+++ nested-classes.rb
@@ -0,0 +1,10 @@
+class Outer
+    def test_outer
+
+    end
+  class Inner
+    def test_inner
+
+    end
+  end
+end
